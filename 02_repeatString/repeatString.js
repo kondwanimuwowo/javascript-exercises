08bee7f84bbd4e4156ab4ef1b08ccac12c16a52f
Kondwani Muwowo <168133978+kondwanimuwowo@users.noreply.github.com>
2024-09-28 12:56:13 +0300

    Correct typo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -5,7 +5,7 @@ const repeatString = function(string, num) {
     }
     return result;
 };
-repeatString("h1", 1);
+repeatString("hi", 1);
 
 // Do not edit below this line
 module.exports = repeatString;
