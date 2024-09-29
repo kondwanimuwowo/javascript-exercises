82dd14f8aef49174e0a008d9341bb25dca1a45cd
Kondwani Muwowo <168133978+kondwanimuwowo@users.noreply.github.com>
2024-09-29 14:29:24 +0300

    Passed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,18 @@
 const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    
     result = "";
     for (let i = 0; i < num; i++) {
         result += string;
-    }
-    if (num < 0) {
-        return "ERROR";
-    } else {
-        return result;
-    }
+    
+    return result;
+
 };
-repeatString("goodbye", 0);
+repeatString("hello", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
