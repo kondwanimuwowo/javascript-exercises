2c818a083a4b8634e7611cd4fefe42b0a232190b
Kondwani Muwowo <168133978+kondwanimuwowo@users.noreply.github.com>
2024-09-29 17:52:43 +0300

    3 tests passed, 1 skipped

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -8,7 +8,7 @@ for (let i = 0; i < arrayRev.length; i++) {
 }
 return result;
 };
-reverseString("hello there");
+reverseString("123! abc! Hello, Odinite.");
 
 // Do not edit below this line
 module.exports = reverseString;
